dc4132f4d37b9143bbb15889608c06b17077c3c6
nehamaria <[nehamariasanil@gmail.com]>
2021-11-01 16:07:53 +0530

    Created new rails application

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Quizzy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
   end
 end
