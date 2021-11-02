f9d1172c1b544711900f479f2b1cde70995686cf
nehamaria <[nehamariasanil@gmail.com]>
2021-11-02 20:15:27 +0530

    Updated database.yml.ci

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+import { Header } from "@bigbinary/neetoui/v2/layouts";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import { setAuthHeaders } from "apis/axios";
@@ -17,6 +18,7 @@ const App = () => {
 
   return (
     <Router>
+      <Header title="Quizzy" className="border-b-2" />
       <Switch>
         <Route exact path="/" render={() => <div>Home</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
