834e5d96b75c9ebfa5fc920768fd553c624581ff
nehamaria <[nehamariasanil@gmail.com]>
2021-11-12 22:55:49 +0530

    Renamed opt to option in ShowQuestionList in PR #37

--- app/javascript/src/components/Quiz/ShowQuiz/ShowQuestionList/Options.jsx
+++ app/javascript/src/components/Quiz/ShowQuiz/ShowQuestionList/Options.jsx
@@ -8,14 +8,14 @@ const Options = ({ optionList }) => {
 
   return (
     <div className="space-y-4">
-      {options.map((opt, index) => {
+      {options.map((option, index) => {
         return (
           <div key={index} className="flex space-x-10 bg-white p-2 ">
             <Typography style="body2" className="ml-10 flex-shrink-0">
               Option {index + 1}
             </Typography>
-            <Typography style="body2">{opt.name}</Typography>
-            {opt.correct_answer && (
+            <Typography style="body2">{option.name}</Typography>
+            {option.correct_answer && (
               <div className="flex flex-shrink-0">
                 <CheckCircle color="#00ba88" />
                 <Typography style="body2" className="neeto-ui-text-success">
