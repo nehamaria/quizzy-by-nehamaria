f89ddc7fb8ecf42efbb914188f74a8c2cee86332
nehamaria <[nehamariasanil@gmail.com]>
2021-11-13 15:45:09 +0530

    Made changes in routes.rb as per PR #37

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   defaults format: :json do
     resource :sessions, only: %i[create destroy]
-    resources :quizzes, only: %i[create index destroy update show], param: :id do
+    resources :quizzes, only: %i[create index destroy update show] do
      resources :questions, only: %i[create destroy show update]
    end
   end
